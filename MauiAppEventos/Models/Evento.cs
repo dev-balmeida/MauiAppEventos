ead54b5ba3d275f2b6311a8f0f028a66dc5f58ff
dev-balmeida <beatrizdealmeida00@outlook.com>
2024-11-15 18:15:13 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppEventos/Models/Evento.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppEventos.Models
+{
+    internal class Evento
+    {
+        public string NomeDoEvento { get; set; }
+        public DateTime DataInicio { get; set; }
+        public DateTime DataTermino { get; set; }
+        public int NumeroDeParticipante { get; set; }
+        public string LocalDoEvento { get; set; }
+        public double CustoPorPaciente { get; set; }
+        public int DuracaoDoEvento
+        {
+            get => DataTermino.Subtract(DataInicio).Days;
+
+        }
+        public double ValorTotal
+        {
+            get
+            {
+                double custo_total_do_evento = NumeroDeParticipante * CustoPorPaciente;
+
+                double total = custo_total_do_evento * DuracaoDoEvento;
+
+                return total;
+            }
+        }
+    }
+}
---
+++ MauiAppEventos/Views/CadastroDeEvento.xaml.cs
@@ -0,0 +1,52 @@
+using MauiAppEventos.Models;
+
+namespace MauiAppEventos.Views;
+
+public partial class CadastroDeEvento : ContentPage
+{
+	public CadastroDeEvento()
+	{
+		InitializeComponent();
+
+        data_inicio.MinimumDate = DateTime.Now;
+        data_inicio.MaximumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month + 1, DateTime.Now.Day);
+
+        data_termino.MinimumDate = data_inicio.Date.AddDays(1);
+        data_termino.MaximumDate = data_inicio.Date.AddMonths(6);
+    }
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Evento h = new Evento
+            {
+                NomeDoEvento = nome_evento.Text,
+                NumeroDeParticipante = Convert.ToInt32(numero_participantes.Value),
+                DataInicio = data_inicio.Date,
+                DataTermino = data_termino.Date,
+                LocalDoEvento = local_evento.Text,
+                CustoPorPaciente = Convert.ToInt32(custo_paciente.Text)
+            };
+
+            await Navigation.PushAsync(new DetalhesDoEvento()
+            {
+                BindingContext = h
+            });
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+
+    private void data_inicio_DateSelected(object sender, DateChangedEventArgs e)
+    {
+        DatePicker elemento = sender as DatePicker;
+
+        DateTime data_selecionada_checkin = elemento.Date;
+
+        data_termino.MinimumDate = data_selecionada_checkin.AddDays(1);
+        data_termino.MaximumDate = data_selecionada_checkin.AddMonths(6);
+    }
+}
